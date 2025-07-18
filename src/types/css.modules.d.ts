2644339d784042e637d3bef2dfe180a1e237ff76
TMaltseva <bacardeonie@gmail.com>
2025-07-18 14:42:53 -0700

    chore: add css modules declaration

---
+++ src/types/css.modules.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Декларация типов для CSS модулей
+ */
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.sass' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
