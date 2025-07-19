57598a9a146ddd875975a0441df9818db898e3aa
TMaltseva <bacardeonie@gmail.com>
2025-07-19 03:01:33 -0700

    feat: add search result page

---
+++ src/pages/SearchResults.tsx
@@ -0,0 +1,69 @@
+import React, { useMemo } from 'react';
+import { useSearchParams, Outlet } from 'react-router-dom';
+import SearchBar from '../components/SearchBar';
+
+// Material-UI компоненты
+import { Container, Box } from '@mui/material';
+import RepositoryTable from '../components/RepositoryTable';
+import {
+  isValidSortOption,
+  isValidSortDirection,
+  parseIntSafely
+} from '../utils/helpers';
+import type { SortOption, SortDirection } from '../types/repo';
+
+import styles from './SearchResults.module.scss';
+
+const SearchResults: React.FC = () => {
+  const [searchParams] = useSearchParams();
+
+  const searchOptions = useMemo(() => {
+    const query = searchParams.get('q') || '';
+    const sortParam = searchParams.get('sort');
+    const orderParam = searchParams.get('order');
+    const pageParam = searchParams.get('page');
+    const perPageParam = searchParams.get('per_page');
+
+    const sort: SortOption = isValidSortOption(sortParam) ? sortParam : 'stars';
+    const order: SortDirection = isValidSortDirection(orderParam)
+      ? orderParam
+      : 'desc';
+    const page = parseIntSafely(pageParam, 1);
+    const per_page = parseIntSafely(perPageParam, 10);
+
+    return {
+      query,
+      sort,
+      order,
+      page,
+      per_page
+    };
+  }, [searchParams]);
+
+  return (
+    <>
+      {/* Хедер с поиском */}
+      <Box className={styles.searchHeader}>
+        <Container maxWidth="xl" className={styles.searchContainer}>
+          <SearchBar placeholder="Введите поисковый запрос" />
+        </Container>
+      </Box>
+
+      {/* Основной контент */}
+      <Container maxWidth="xl" className={styles.container}>
+        <Box className={styles.content}>
+          <Box className={styles.tableSection}>
+            <RepositoryTable searchParams={searchOptions} />
+          </Box>
+
+          {/* Outlet для отображения деталей репозитория */}
+          <Box className={styles.detailsSection}>
+            <Outlet />
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default SearchResults;
